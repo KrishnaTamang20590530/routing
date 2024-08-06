95430b87fdd6e784192a18f7a4fecaec6b7e70f1
Krishna Tamang <tmgkrishna78@gmail.com>
2024-08-06 22:43:43 +1000

    adding demo routes

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+const page = ({params}) => {
+  return (
+    <div>
+      <p>This is the product id {params.productId}</p>
+    </div>
+  )
+}
+
+export default page
---
+++ app/products/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+const page = () => {
+  return (
+    <div>
+      Product Listing
+
+      <Link href= 'products/1'>
+        1
+      </Link>
+    </div>
+  )
+}
+
+export default page
